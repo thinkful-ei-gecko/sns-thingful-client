7992ab9d73ff2fa4e8fc4cab5fca2a339a83d7e7
Scott Schroeder <snschroeder89@gmail.com>
2019-10-11 14:02:44 -0500

    added jwt

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -2,7 +2,6 @@ import config from '../config'
 
 const TokenService = {
   saveAuthToken(token) {
-    console.log(token)
     window.localStorage.setItem(config.TOKEN_KEY, token)
   },
   getAuthToken() {
